bfaa810b0a4b6c43af7d026d980938a140819412
TavoSalcedoMTZ <150973163+TavoSalcedoMTZ@users.noreply.github.com>
2025-03-26 23:55:33 -0600

    Camara fix

--- Assets/Scripts/Player/RotationCamara.cs
+++ Assets/Scripts/Player/RotationCamara.cs
@@ -20,9 +20,9 @@ public class RotationCamara : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        // Aplicar rotación en el eje X (arriba y abajo)
+
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Limitar el rango de la cámara
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); 
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
